4f9befdc06a33dcb60d6d1c451b22af8937109b5
Behzad Afrasiabi <behzad120048@gmail.com>
2024-06-14 07:55:29 -0400

    createStackNode() created and push() is almost done

--- m2.cpp
+++ m2.cpp
@@ -14,7 +14,9 @@
 
 
 #include<stdio.h>
-
+#include<malloc.h>
+#include<stdbool.h>
+#include<stdlib.h>
 
 //RETURN CODES
 #define SUCCESS 0
@@ -29,7 +31,7 @@ typedef struct
 
 typedef struct
 {
-	struct StackNode* top;
+	StackNode* top;
 }Stack;
 
 //PROTOTYPES
@@ -37,11 +39,12 @@ void push(Stack* stack, char* newUrl); //add url to stack
 char* pop(Stack* stack); //removes and returns top stack url
 char* peek(Stack* stack); // return url from top of stack
 bool isEmpty(Stack* stack); //check if stack is empty
+StackNode* createStackNode(void); //creates new StackNode object
 
 //MAIN
 int main(void)
 {
-	
+	Stack* newStack = (Stack*)malloc(sizeof(Stack));
 
 
 	return	SUCCESS;
@@ -49,15 +52,60 @@ int main(void)
 
 
 //
+// FUNCTION :  createStackNode
+// DESCRIPTION :
+//	Creates a new StackNode object.
+// PARAMETERS :
+//	void: no params.
+// RETURNS :
+//	StackNode*: Pointer to the new StackNode object
+//
+StackNode* createStackNode(void)
+{
+	StackNode* node = (StackNode*)malloc(sizeof(StackNode));
+	if (node == NULL)
+	{//exception
+		printf("Memory allocation failure for StackNode* node");
+		return NULL;
+	}
+
+	char* url = (char*)malloc(sizeof(char*));
+	if (url == NULL)
+	{//exception
+		printf("Memory allocation failure for char* url");
+		free(node);
+		return NULL;
+	}
+
+	node->next = NULL;
+	node->url = url;
+
+	return node;
+}
+
+
+//
 // FUNCTION :  push
 // DESCRIPTION :
 //	Adds a new url to the stack.
 // PARAMETERS :
-//	struct FlightInfo* head : the head of the FlightInfo double linked list.
+//	Stack* stack : stack object representing the stack 
+//	the url is to be added to.
+//  char* newUrl: new url to be added to the stack.
 // RETURNS :
 //	void: This function returns nothing.
 //
 void push(Stack* stack, char* newUrl)
 {
+	StackNode* newNode = createStackNode();
+	if (newNode == NULL)
+	{
+		printf("error creating new node");
+	}
+	else
+	{
+		newNode->url = newUrl;
+		stack->top = newNode;
+	}
 
 }
