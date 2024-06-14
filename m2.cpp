b075db2e7e94ab477949f3d7c2ba3c31cbdd0ad8
Behzad Afrasiabi <behzad120048@gmail.com>
2024-06-14 07:18:06 -0400

    function prototypes for stack complete

--- m2.cpp
+++ m2.cpp
@@ -32,11 +32,32 @@ typedef struct
 	struct StackNode* top;
 }Stack;
 
+//PROTOTYPES
+void push(Stack* stack, char* newUrl); //add url to stack
+char* pop(Stack* stack); //removes and returns top stack url
+char* peek(Stack* stack); // return url from top of stack
+bool isEmpty(Stack* stack); //check if stack is empty
 
+//MAIN
 int main(void)
 {
-
+	
 
 
 	return	SUCCESS;
+}//MAIN END
+
+
+//
+// FUNCTION :  push
+// DESCRIPTION :
+//	Adds a new url to the stack.
+// PARAMETERS :
+//	struct FlightInfo* head : the head of the FlightInfo double linked list.
+// RETURNS :
+//	void: This function returns nothing.
+//
+void push(Stack* stack, char* newUrl)
+{
+
 }
