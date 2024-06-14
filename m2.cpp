db6e5df054bf59b41b0a3765a236f94809b8ca23
Behzad Afrasiabi <behzad120048@gmail.com>
2024-06-14 07:09:43 -0400

    added header and struct defs

---
+++ m2.cpp
@@ -0,0 +1,42 @@
+/*
+* FILE : m2.cpp
+* PROJECT : SENG1050 - Assignment #2
+* PROGRAMMER : Behzad Afrasiabi
+* FIRST VERSION : 2024-06-14
+* DESCRIPTION :
+*	This program uses stacks as url. 
+* 	  FILL LATER
+* 	  FILL LATER
+* 	  FILL LATER
+* 	  FILL LATER
+* 	  FILL LATER
+*/	 
+
+
+#include<stdio.h>
+
+
+//RETURN CODES
+#define SUCCESS 0
+#define FAILURE -1
+
+//STRUCTS
+typedef struct 
+{
+	char* url;
+	struct StackNode* next;
+}StackNode;
+
+typedef struct
+{
+	struct StackNode* top;
+}Stack;
+
+
+int main(void)
+{
+
+
+
+	return	SUCCESS;
+}
